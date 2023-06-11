6d77d5a7d34bc51f7e0aa3556a7675fcc12c1a4d
smastrom <60471784+smastrom@users.noreply.github.com>
2023-06-11 20:34:29 +0200

    merge CombinationListView with Combinations

--- components/app/routes/Combinations.vue
+++ components/app/routes/Combinations.vue
@@ -1,58 +1,159 @@
 <script setup lang="ts">
-import { APP_CRITICAL_ERROR } from '@/lib/constants'
 import { db } from '@/lib/db'
-import { useUpdateQuery } from '@/lib/useUpdateQuery'
 
-import NewCombinationView from '@/components/shared/NewCombinationView.vue'
-import CombinationListView from '@/components/shared/CombinationListView.vue'
+import Preview from '@/components/shared/Preview.vue'
+import ContentLayout from '@/components/shared/ContentLayout.vue'
+import CombinationListHeader from '@/components/shared/CombinationListHeader.vue'
+import CombinationListCreate from '@/components/shared/CombinationListCreate.vue'
+import CombinationListEntry from '@/components/shared/CombinationListEntry.vue'
+
+import { APP_CRITICAL_ERROR, SORT_CRITERIA } from '@/lib/constants'
+import { getMemoryOrDBInstanceKey, injectFontFace, injectedFonts } from '@/lib/injectFonts'
+import { getFamily } from '@/lib/fonts'
+import { useStore } from '@/lib/store'
 
 import type { DBCombination } from '@/types/db'
+import { reloadPage } from '@/lib/utils'
 
-const route = useRoute()
-const updateQuery = useUpdateQuery()
-
-const shouldRenderCreateDialog = ref(true)
-const combinationsRef = ref<DBCombination[] | null>(null)
-
-function setCombinationsRef(combinations: DBCombination[]) {
-   combinationsRef.value = combinations
+const entries = await db.getAll()
+if (!entries) {
+   throw new Error(`[combination-list-view] - Failed fetching combinations! ${APP_CRITICAL_ERROR}`)
 }
 
-// Check which view to render
+const route = useRoute()
+const router = useRouter()
+const store = useStore()
 
+const combinations = ref<DBCombination[]>(entries)
+const activeEntry = ref(combinations.value[0])
+
+// 1. Restore active value restore from query
 try {
-   const combinations = await db.getAll()
+   if (route.query.current) {
+      const id = route.query.current
+      const entry = await db.get(id as string)
 
-   if (route.query.new) {
-      // Returning user trying to access /combinations?new
-      if (combinations && combinations.length > 0) {
-         await updateQuery('new', undefined)
-         shouldRenderCreateDialog.value = false
-         combinationsRef.value = combinations
-      } // Else render with default props
-   } else {
-      // New user trying to access /combinations
-      if (!combinations || combinations.length === 0) {
-         await updateQuery('new', 'true')
-         shouldRenderCreateDialog.value = true
-      } else {
-         // Returning user trying to access /combinations
-         shouldRenderCreateDialog.value = false
-         combinationsRef.value = combinations
+      if (entry) {
+         const currId = combinations.value.find(({ id: dbId }) => dbId === id)
+         if (currId) activeEntry.value = currId
       }
    }
 } catch (error) {
-   throw new Error(`[combinations-route-db] - ${APP_CRITICAL_ERROR}`)
+   throw new Error(
+      `[combination-list-view] - Failed setting latest active ID! ${APP_CRITICAL_ERROR}`
+   )
 }
 
-console.log(combinationsRef.value)
+// 2. If no fonts in the store (new visit), fetch them. Here we don't care about the sort,
+if (!store.fonts.data.value) await store.fonts.actions.fetchAndSetFonts(SORT_CRITERIA[0].value)
+if (!store.fonts.data.value) {
+   throw new Error(`[combination-list-view] - Failed fetching fonts! ${APP_CRITICAL_ERROR}`)
+}
+
+// 3. Inject fonts and weights needed for the preview
+try {
+   for (const { headline, body } of combinations.value) {
+      for (const { family, weight } of [headline, body]) {
+         const key = getMemoryOrDBInstanceKey(family, weight)
+
+         // 1. Exit if in memory
+         if (injectedFonts.has(key)) continue
+
+         // 2. Fetch from DB / Google
+         const fontUrl = getFamily(store.fonts.data.value, family).files[weight]
+         const buffer = await db.getFontBuffer(key, fontUrl)
+
+         // 3. Inject FontFace
+         await injectFontFace({ key, family, weight, buffer })
+      }
+   }
+} catch (error) {
+   throw new Error(`[combination-list-view] - Failed injecting fonts! ${APP_CRITICAL_ERROR}`)
+}
+
+// 5. Set current preview fonts, at this point Suspense resolves
+
+store.preview.actions.setActiveId(activeEntry.value.id)
+store.preview.actions.setBodyFont(activeEntry.value.body)
+store.preview.actions.setHeadlineFont(activeEntry.value.headline)
+
+// Events
+
+async function onDelete(id: string) {
+   try {
+      const newCombinations = combinations.value.filter((entry) => entry.id !== id)
+      // If deleting last combination
+      if (newCombinations.length === 0) {
+         await router.replace({ query: {} }) // Remove query
+         await db.remove(id) // Delete it from DB, do not update the UI
+
+         reloadPage() // Route guard will send to first-combination if no combinations left
+      } else {
+         combinations.value = newCombinations
+         await db.remove(id) // Delete in background
+      }
+   } catch (error) {
+      console.error(error) // Do not show error, just log it
+   }
+}
 </script>
 
 <template>
-   <NewCombinationView v-if="shouldRenderCreateDialog" />
-   <CombinationListView
-      :combinations="combinationsRef"
-      :setCombinations="setCombinationsRef"
-      v-else
-   />
+   <CombinationListHeader />
+
+   <ContentLayout>
+      <!-- Explorer -->
+
+      <template #explorer>
+         <div class="CombinationList_Explorer">
+            <h2 class="Preview_Title">My Combinations</h2>
+
+            <CombinationListCreate />
+
+            <div class="CombinationList_Grid">
+               <CombinationListEntry
+                  v-for="combination in combinations"
+                  :key="combination.id"
+                  :combination="combination"
+                  @delete="onDelete(combination.id)"
+               />
+            </div>
+         </div>
+      </template>
+
+      <!-- Preview -->
+
+      <template #preview>
+         <div class="CombinationList_Explorer">
+            <div>
+               <h2 class="Preview_Title">Live Preview</h2>
+            </div>
+            <Preview />
+         </div>
+      </template>
+   </ContentLayout>
 </template>
+
+<style scoped>
+.Preview_Title {
+   font-weight: 700;
+   color: var(--fg-headline-color);
+   font-size: var(--font-size-4);
+}
+
+.CombinationList_Explorer {
+   overflow: auto;
+   display: flex;
+   flex-direction: column;
+}
+
+.CombinationList_Grid {
+   display: grid;
+   margin-bottom: var(--size-4);
+   overflow: auto;
+
+   & > div:last-of-type {
+      border: none;
+   }
+}
+</style>
